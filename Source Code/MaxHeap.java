369ec468c8dcc1cf8fa478633e6732e6f5a6c5c6
Jonathan Pena <98781952+JonathanPena17@users.noreply.github.com>
2022-04-20 16:19:23 -0700

    Update MaxHeap.java

--- Source Code/MaxHeap.java
+++ Source Code/MaxHeap.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 /**
    A class that implements the ADT maxheap by using an array.
  
@@ -6,233 +5,182 @@ import java.util.Arrays;
    @author Timothy M. Henry
    @version 5.0
 */
-public final class MaxHeap<T extends Comparable<? super T>>
-             implements MaxHeapInterface<T>
-{
-   private T[] heap;      // Array of heap entries; ignore heap[0]
-   private int lastIndex; // Index of last entry and number of entries
-   private boolean integrityOK = false;
-   private static final int DEFAULT_CAPACITY = 25;
-   private static final int MAX_CAPACITY = 10000;
-   
-   private int swapCounter = 0;
-   
-   public MaxHeap()
-   {
-      this(DEFAULT_CAPACITY); // Call next constructor
-   } // end default constructor
-   
-   public MaxHeap(int initialCapacity)
-   {
-      // Is initialCapacity too small?
-      if (initialCapacity < DEFAULT_CAPACITY)
-          initialCapacity = DEFAULT_CAPACITY;
-      else // Is initialCapacity too big?
-          checkCapacity(initialCapacity);
-      
-      // The cast is safe because the new array contains null entries
-      @SuppressWarnings("unchecked")
-      T[] tempHeap = (T[])new Comparable[initialCapacity + 1];
-      heap = tempHeap;
-      lastIndex = 0;
-      integrityOK = true;
-   } // end constructor
+public final class MaxHeap<T extends Comparable<? super T>> implements MaxHeapInterface<T>{
+    
+    private final T[] heap; // Array of heap entries; ignore heap[0]
+    private int lastIndex; // Index of last entry and number of entries
+    private boolean initialized = false;
+    private static final int DEFAULT_CAPACITY = 25;
+    private static final int MAX_CAPACITY = 10000;
 
-   // Section 27.16 (max heap constructor)
-   public MaxHeap(T[] entries)
-   {
-      this(entries.length); // Call other constructor
-      lastIndex = entries.length;
-      // Assertion: integrityOK = true
+    private int swapCounter = 0;
 
-      // Copy given array to data field
-      for (int index = 0; index < entries.length; index++)
-         heap[index + 1] = entries[index];
+    public MaxHeap()
+    {
+        this(DEFAULT_CAPACITY); // Call next constructor
+    } // end default constructor
 
-      // Create heap
-      for (int rootIndex = lastIndex / 2; rootIndex > 0; rootIndex--)
-         reheap(rootIndex);
-   } // end constructor
+    public MaxHeap(int initialCapacity)
+    {
+         // Is initialCapacity too small?
+        if (initialCapacity < DEFAULT_CAPACITY)
+            initialCapacity = DEFAULT_CAPACITY;
+        else // Is initialCapacity too big?
+            checkCapacity(initialCapacity);
 
-   // Section 27.20 (heap sort method)
-   public static <T extends Comparable<? super T>>
-       void heapSort(T[] array, int n)
-   {
-      // Create first heap
-      for (int rootIndex = n / 2 - 1; rootIndex >= 0; rootIndex--)
-         reheap(array, rootIndex, n - 1);
+         // The cast is safe because the new array contains null entries
+        @SuppressWarnings("unchecked")
+        T[] tempHeap = (T[]) new Comparable[initialCapacity + 1];
+        heap = tempHeap;
+        lastIndex = 0;
+        initialized = true;
+    } // end constructor
 
-      swap(array, 0, n - 1);
+     // Section 27.16 (max heap constructor)
+    public MaxHeap(T[] entries)
+    {
+        this(entries.length); // Call other constructor
+        lastIndex = entries.length;
+        assert initialized = true;  // Assertion: integrityOK = true
+ 
+        // Copy given array to data field
+        for(int index = 0; index < entries.length; index++)
+            heap[index + 1] = entries[index];
+        
+        // Create heap
+        for(int rootIndex = lastIndex / 2; rootIndex > 0; rootIndex --)
+            reheap(rootIndex);
+    } // end constructor
 
-      for (int lastIndex = n - 2; lastIndex > 0; lastIndex--)
-      {
-         reheap(array, 0, lastIndex);
-         swap(array, 0, lastIndex);
-      } // end for
-   } // end heapSort
+    // Section 27.8 (add method) (See Segment 27.8 - original)
+    public void add(T newEntry)
+    {
+        checkInitialization();
+        int newIndex = lastIndex + 1;
+        int parentIndex = newIndex / 2;
+        
+        while ( (parentIndex > 0) && newEntry.compareTo(heap[parentIndex]) > 0)
+        {
+            heap[newIndex] = heap[parentIndex];
+            newIndex = parentIndex;
+            parentIndex = newIndex / 2;
 
-   private static final <T> void swap(T[]array, int i, int j) {
-      T temp = array[i];
-      array[i] = array[j];
-      array[j] = temp; 
-   } // end swap
+            swapCounter++;
+        }// end while
 
-   // Section 27.8 (add method) (See Segment 27.8 - original)
-   public void add(T newEntry)
-   {
-      checkIntegrity();        // Ensure initialization of data fields
-      int newIndex = lastIndex + 1;
-      int parentIndex = newIndex / 2;
-      while ( (parentIndex > 0) && newEntry.compareTo(heap[parentIndex]) > 0)
-      {
-         heap[newIndex] = heap[parentIndex];
-         newIndex = parentIndex;
-         parentIndex = newIndex / 2;
-      } // end while
+        heap[newIndex] = newEntry;
+        lastIndex++;
+        ensureCapacity();
+    }  // end add
 
-      heap[newIndex] = newEntry;
-      lastIndex++;
-      ensureCapacity();
-   } // end add
+     // Section 27.12 (removeMax method/ remove method of a max-heap) (See Segment 27.12 - original)
+    public T removeMax()
+    {
+        checkInitialization(); // Ensure initialization of data fields
+        T root = null;
 
-   // Section 27.12 (removeMax method/ remove method of a max-heap) (See Segment 27.12 - original)
-   public T removeMax()
-   {
-      checkIntegrity();             // Ensure initialization of data fields
-      T root = null;
+        if (!isEmpty())
+        {
+            root = heap[1];            // Return value
+            heap[1] = heap[lastIndex]; // Form a semiheap
+            lastIndex--;               // Decrease size
+            reheap(1);        // Transform to a heap
+        } // end if 
 
-      if (!isEmpty())
-      {
-         root = heap[1];            // Return value
-         heap[1] = heap[lastIndex]; // Form a semiheap
-         lastIndex--;               // Decrease size
-         reheap(1);                 // Transform to a heap
-      } // end if
+        return root;
+    } // end removeMax
 
-      return root;
-   } // end removeMax
+    public T getMax()
+    {
 
-   public T getMax()
-   {
-		checkIntegrity();
-      T root = null;
-      if (!isEmpty())
-         root = heap[1];
-      return root;
-   } // end getMax
+        checkInitialization();
+        T root = null;
+        if (!isEmpty())
+            root = heap[1];
 
-   public boolean isEmpty()
-   {
-      return lastIndex < 1;
-   } // end isEmpty
+        return root;
+    } //end getMax
 
-   public int getSize()
-   {
-      return lastIndex;
-   } // end getSize
+    public boolean isEmpty()
+    {
+        return lastIndex < 1;
+    } // end isEmpty
 
-   public void clear()
-   {
-		checkIntegrity();
-      while (lastIndex > -1)
-      {
-         heap[lastIndex] = null;
-         lastIndex--;
-      } // end while
-      lastIndex = 0;
-   } // end clear
-   
+    public int getSize()
+    {
+        return lastIndex;
+    } // end getSize
+
+    public void clear()
+    {
+
+        checkInitialization();
+        while (lastIndex > -1)
+        {
+            heap[lastIndex] = null;
+            lastIndex--;
+        } // end while
+
+        lastIndex = 0;
+    } // end clear
+
+    public void checkCapacity(int topIndex)
+    {
+        if (topIndex > MAX_CAPACITY)
+            throw new SecurityException("Array MaxHeap has exceeded maximum capacity.");
+    } // end checkCapacity
+
+    public void checkInitialization()
+    {
+        if (!this.initialized){
+            throw new SecurityException("Array MaxHeap is corrupt.");
+        }
+    } // end checkInitialization
+
+    public void ensureCapacity()
+    {
+        if ((lastIndex * 2) > heap.length)
+            throw new SecurityException("Array MaxHeap not currently large enough.");
+    } // end ensureCapacity
+
+    public int getSwap()
+    {
+        return swapCounter;
+    } // end getswap
+
+    public void toString(int x)
+    {
+        for(int i = 1; i <= x; i++)
+            System.out.print(heap[i] + ",");
+    } // end toString
+    
    // Private methods
-
    // Section 27.11 (reheap with precondition)
    // Precondition: checkIntegrity has been called.
-   private void reheap(int rootIndex)
-   {
-      boolean done = false;
-      T orphan = heap[rootIndex];
-      int leftChildIndex = 2 * rootIndex;
+    private void reheap(int rootIndex) 
+    {
+        boolean done = false;
+        T orphan = heap[rootIndex];
+        int leftChildIndex = 2 * rootIndex;
 
-      while (!done && (leftChildIndex <= lastIndex) )
-      {
-         int largerChildIndex = leftChildIndex; // Assume larger
-         int rightChildIndex = leftChildIndex + 1;
+        while (!done && (leftChildIndex <= lastIndex)) 
+        {
+            int largerChildIndex = leftChildIndex;
+            int rightChildIndex = leftChildIndex + 1;
+            if ((rightChildIndex <= lastIndex) && heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0) 
+            {
+                largerChildIndex = rightChildIndex;
+            }
+            if (orphan.compareTo(heap[largerChildIndex]) < 0) 
+            {
+                heap[rootIndex] = heap[largerChildIndex];
+                rootIndex = largerChildIndex;
+                leftChildIndex = 2 * rootIndex;
+                swapCounter++;
+            } else
+                done = true;
+        }
+        heap[rootIndex] = orphan;
+    }
 
-         if ( (rightChildIndex <= lastIndex) &&
-               heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
-         {
-            largerChildIndex = rightChildIndex;
-         } // end if
-
-         if (orphan.compareTo(heap[largerChildIndex]) < 0)
-         {
-            heap[rootIndex] = heap[largerChildIndex];
-            rootIndex = largerChildIndex;
-            leftChildIndex = 2 * rootIndex;
-         }
-         else
-            done = true;
-      } // end while
-
-      heap[rootIndex] = orphan;
-   } // end reheap
-
-   // Section 27.19 (reheap method with 3 input fields)
-   private static <T extends Comparable<? super T>>
-        void reheap(T[] heap, int rootIndex, int lastIndex)
-   {
-      boolean done = false;
-      T orphan = heap[rootIndex];
-      int leftChildIndex = 2 * rootIndex + 1;
-
-      while (!done && (leftChildIndex <= lastIndex))
-      {
-         int largerChildIndex = leftChildIndex;
-         int rightChildIndex = leftChildIndex + 1;
-
-         if ( (rightChildIndex <= lastIndex) &&
-               heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
-         {
-            largerChildIndex = rightChildIndex;
-         } // end if
-
-         if (orphan.compareTo(heap[largerChildIndex]) < 0)
-         {
-            heap[rootIndex] = heap[largerChildIndex];
-            rootIndex = largerChildIndex;
-            leftChildIndex = 2 * rootIndex + 1;
-         }
-         else
-            done = true;
-      } // end while
-
-      heap[rootIndex] = orphan;
-   } // end reheap
-
-   private void ensureCapacity()
-   {
-      if(lastIndex >= heap.length - 1)
-      {
-         int newLength = 2 * heap.length;
-         checkCapacity(lastIndex);
-         heap = Arrays.copyOf(heap, newLength);
-      }
-   } // end ensureCapacity
-
-   private void checkIntegrity()
-   {
-      if(!integrityOK)
-      {
-         throw new SecurityException("ArrayList Object is corrupt.");
-      }
-   } // end checkIntegrity
-
-   private void checkCapacity(int initialCapacity)
-   {
-      if(initialCapacity > MAX_CAPACITY)
-      {
-         throw new ArrayIndexOutOfBoundsException("Array capacity has exceeded maximum capacity.");
-      }
-   } // end checkCapacity
-
-// . . .
-} // end MaxHeap
+}
