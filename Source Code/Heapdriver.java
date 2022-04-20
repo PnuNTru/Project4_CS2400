fdfe21c02a22c8a93300d7a8ac8c15975b4335bd
Jonathan Pena <98781952+JonathanPena17@users.noreply.github.com>
2022-04-20 15:10:38 -0700

    Update Heapdriver.java

--- Source Code/Heapdriver.java
+++ Source Code/Heapdriver.java
@@ -6,6 +6,12 @@ public class Heapdriver
 {
   public static void main (String [] args) throws FileNotFoundException
   {
+    System.out.println("=====================================================================");
+    
+    String fileName =  "data_sorted.txt";
+    generateHeap(filename);
+    
+    fileName = "data_random.txt"
     
   }
 }
